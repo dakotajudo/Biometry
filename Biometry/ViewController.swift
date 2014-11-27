c2ea9178739d5d0d634006431b06a110228e721d
Peter Claussen <dakotajudo@mac.com>
2014-11-27 17:57:29 -0600

    Initial Commit

---
+++ Biometry/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Biometry
+//
+//  Created by Peter Claussen on 11/27/14.
+//  Copyright (c) 2014 Peter Claussen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+   }
+
+   override var representedObject: AnyObject? {
+      didSet {
+      // Update the view, if already loaded.
+      }
+   }
+
+
+}
+
